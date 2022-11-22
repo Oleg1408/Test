6a454ca79bd29b20554aea24b51da375ffb09620
Oleg Kurbatov <olegkurbatov1408@gmail.com>
2022-11-22 17:43:16 +0200

    first commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Олег Курбатов on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+        
+        
+    }
+
+}
+
